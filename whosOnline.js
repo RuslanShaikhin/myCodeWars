cbddefa6a803648cc5e83751ac7d2a71ed35f920
Ruslan Shaikhin <shaikhin1996@gmail.com>
2024-09-03 19:50:40 +0200

    whosOnline

---
+++ whosOnline.js
@@ -0,0 +1,52 @@
+// done, no test
+    
+    let friends = [{
+    username: 'David',
+    status: 'online',
+    lastActivity: 1
+    }, {
+    username: 'Lucy', 
+    status: 'online',
+    lastActivity: 1
+    }, {
+    username: 'Bob', 
+    status: 'online',
+    lastActivity: 1
+    }]
+
+
+const whosOnline = (friends) => {
+    if(friends.length===0) {
+        return {};
+    }
+
+    let result = {
+        online: [],
+        offline:[],
+        away:[]
+    }
+
+    for (let i = 0; i < friends.length; i++) {
+        if(friends[i].status === 'offline') {
+            result.offline.push(friends[i].username);
+        } else if(friends[i].status === 'online' && friends[i].lastActivity <= 10) {
+            result.online.push(friends[i].username);
+        } else {
+            result.away.push(friends[i].username);
+        }
+    }
+
+    if(result.online.length===0) {
+        delete result.online;
+    }
+    if(result.offline.length===0) {
+        delete result.offline;
+    }
+    if(result.away.length===0) {
+        delete result.away;
+    }
+
+    return result;
+}
+
+console.log(whosOnline(friends));
